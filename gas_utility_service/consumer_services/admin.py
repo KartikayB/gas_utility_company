dce1dca4aa94a71cb6251e5a466ea546ef12abd8
KartikayB <kartikay.bansal2014@gmail.com>
2023-08-13 12:19:14 +0530

    Structure chnages for the admin

--- gas_utility_service/consumer_services/admin.py
+++ gas_utility_service/consumer_services/admin.py
@@ -3,7 +3,7 @@ from .models import User_info
 # Register your models here.
 
 class UserComplaintDisplay(admin.ModelAdmin):
-  list_display = ("user", "issue_id", "issue", "status", "date_updated")
+  list_display = ("issue_id", "user", "issue", "status", "date_created", "updated_at", "admin_note")
 
 
 admin.site.register(User_info, UserComplaintDisplay)
--- gas_utility_service/consumer_services/models.py
+++ gas_utility_service/consumer_services/models.py
@@ -1,14 +1,24 @@
 from django.db import models
 import datetime
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class User_info(models.Model):
+    STATUS = [
+        ("ACK", "ACCEPTED"),
+        ("REJ", "REJECTED"),
+        ("INP", "IN PROGRESS"),
+        ("COM", "COMPLETED")
+    ]
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     issue_id = models.AutoField(primary_key=True)
     issue = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     extra_info = models.CharField(max_length=255)
-    status = models.CharField(max_length=15, default='accepted')
-    date_updated = models.DateTimeField(default=datetime.datetime.now)
-    file = models.FileField()
+    status = models.CharField(choices=STATUS,
+        default='ACK',max_length=3)
+    date_created = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    file = models.FileField(blank=True)
+    admin_note = models.CharField(max_length=255, blank=True)
