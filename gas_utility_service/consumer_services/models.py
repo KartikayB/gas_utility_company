2ca2663eb0b014e82eee91558ec09976d26500d9
KartikayB <kartikay.bansal2014@gmail.com>
2023-08-13 13:40:49 +0530

    Data presentation and html update

--- gas_utility_service/consumer_services/models.py
+++ gas_utility_service/consumer_services/models.py
@@ -6,18 +6,18 @@ from django.utils import timezone
 
 class User_info(models.Model):
     STATUS = [
-        ("ACK", "ACCEPTED"),
-        ("REJ", "REJECTED"),
-        ("INP", "IN PROGRESS"),
-        ("COM", "COMPLETED")
+        ("ACCEPTED", "ACCEPTED"),
+        ("REJECTED", "REJECTED"),
+        ("IN-PROGRESS", "IN PROGRESS"),
+        ("COMPLETED", "COMPLETED")
     ]
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     issue_id = models.AutoField(primary_key=True)
     issue = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-    extra_info = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, blank=True)
+    extra_info = models.CharField(max_length=255, blank=True)
     status = models.CharField(choices=STATUS,
-        default='ACK',max_length=3)
+        default='ACCEPTED',max_length=12)
     date_created = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
     file = models.FileField(blank=True)
